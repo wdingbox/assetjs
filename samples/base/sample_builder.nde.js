d31385407c3dfba5f8a5d50cb052a9aab545684a
weiding <wdingbox@gmail.com>
2020-11-04 14:33:01 -0500

    css

--- samples/base/sample_builder.nde.js
+++ samples/base/sample_builder.nde.js
@@ -39,7 +39,7 @@ function gethtm(indir) {
     var str = "", trs = ""
     var arr = Uti.GetFilesAryFromDir(indir, true, function (fname) {
         var sta = fs.statSync(fname)
-        fname = fname.substr(3)
+        fname = fname.substr(3*2)
         trs += `<tr><td></td><td class='fname'>${fname}</td><td>${sta.size}</td><td>${sta.mtime}</td></tr>\n`
         fname = `${urlroot}/${fname}`
         console.log("fname:", fname, sta)
@@ -49,15 +49,15 @@ function gethtm(indir) {
 }
 function main() {
     
-    var ret1 = gethtm("../libs")
-    var ret2 = gethtm("../data")
+    var ret1 = gethtm("../../libs")
+    var ret2 = gethtm("../../data")
 
     var htm = `<html><head>${ret1.str+ret2.str}
     <!------>
     <script src='sample.js' type='application/javascript'></script>
     <link rel="stylesheet" href="sample.css"></link>
     </head><body>
-    <table border='1'><thead><tr><th>#</th><th>file</th><th>size</th><th>mtime</th></tr><thead>\n<tbody id='sels'>${ret1.trs+ret2.trs}</tbody></table>\n<textarea id='out' cols='200' rows='100'>...</textarea>\n</body></html>`
+    <table border='1'><thead><tr><th>#</th><th>file</th><th>size</th><th>mtime</th></tr><thead>\n<tbody id='sels'>${ret1.trs+ret2.trs}</tbody></table>\n<textarea id='out' cols='200' rows='30'>...</textarea>\n</body></html>`
     fs.writeFileSync("sample.htm", htm, "utf8")
 }
 
